05cde2356b4180450661f4032f0063a40edd8054
MohamedNAGYYS <161846669+MohamedNAGYYS@users.noreply.github.com>
2024-03-05 17:58:26 +0200

    int..

---
+++ index.c
@@ -0,0 +1,8 @@
+# include <stsio.h>
+# include <cs50.h>
+
+int main (void) {
+    int num1 = get_int("Enter a number: ");
+    int num2 = get_int("Enter a number: ");
+    printf("%i", num1 + num);
+}
